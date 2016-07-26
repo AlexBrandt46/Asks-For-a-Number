9a8c579f1b05f3ab418fcc3684b79a9b756957d0
AlexBrandt46 <ajb3155@gmail.com>
2016-07-26 11:51:53 -0400

    Took out a pointless line, and added more words to a string.

--- random_number_inputer.rb
+++ random_number_inputer.rb
@@ -1,11 +1,10 @@
 puts "Please put a number between 0 and 100. I'll tell you when you're correct."
 random_number = gets.chomp
 while random_number != "46"
-    puts "Wrong."
     if random_number > "46"
-        puts "Too high."
+        puts "Too high, try again."
     else random_number < "46"
-        puts "Too low."
+        puts "Too low, try again."
     end
     random_number = gets.chomp
 end
